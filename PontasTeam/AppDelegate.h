c485e3274f027465753dd0eb0cba3eade8984c18
André Feliciano <mac.agk@gmail.com>
2016-01-09 23:34:52 -0200

    Initial Commit

---
+++ PontasTeam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PontasTeam
+//
+//  Created by André Feliciano on 09/01/16.
+//  Copyright © 2016 André Feliciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PontasTeam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PontasTeam
+//
+//  Created by André Feliciano on 09/01/16.
+//  Copyright © 2016 André Feliciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
